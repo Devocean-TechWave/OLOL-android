65057cf2f6587610faa39356d1f46bfebd0d92dc
SeyeonJang <seye0n.bliss@gmail.com>
2024-08-03 14:15:28 +0900

    [#1] feat: BottomNavigationView Component 추가

--- app/src/main/java/com/project/olol/presentation/ui/component/BottomNavigationView.kt
+++ app/src/main/java/com/project/olol/presentation/ui/component/BottomNavigationView.kt
@@ -1,2 +1,50 @@
 package com.project.olol.presentation.ui.component
 
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.annotation.DrawableRes
+import com.project.olol.R
+
+sealed class BottomNavItem(
+    var title: String,
+    @DrawableRes var iconSelected: Int,
+    @DrawableRes var iconUnselected: Int
+) {
+    object Mission : BottomNavItem("미션", R.drawable.ic_mission_selected, R.drawable.ic_mission_unselected)
+    object Friends : BottomNavItem("친구", R.drawable.ic_friends_selected, R.drawable.ic_friends_unselected)
+    object Test : BottomNavItem("테스트", R.drawable.ic_test_selected, R.drawable.ic_test_unselected)
+    object My : BottomNavItem("마이", R.drawable.ic_my_selected, R.drawable.ic_my_unselected)
+}
+
+@Composable
+fun NavigationBar(selectedTab: BottomNavItem, onTabSelected: (BottomNavItem) -> Unit) {
+    NavigationBar {
+        val items = listOf(
+            BottomNavItem.Mission,
+            BottomNavItem.Friends,
+            BottomNavItem.Test,
+            BottomNavItem.My
+        )
+        items.forEach { item ->
+            val icon: Painter = painterResource(id = if (selectedTab == item) item.iconSelected else item.iconUnselected)
+            NavigationBarItem(
+                icon = { Icon(painter = icon, contentDescription = item.title) },
+                label = { Text(text = item.title) },
+                selected = selectedTab == item,
+                onClick = { onTabSelected(item) }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NavigationBarPreview() {
+    val selectedTab = remember { mutableStateOf<BottomNavItem>(BottomNavItem.Mission) }
+    NavigationBar(selectedTab.value) { selectedTab.value = it }
+}
--- app/src/main/java/com/project/olol/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/project/olol/presentation/ui/main/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import com.project.olol.presentation.ui.component.BottomNavItem
+import com.project.olol.presentation.ui.component.NavigationBar
 import com.project.olol.presentation.ui.theme.OLOLTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +26,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OLOLTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val selectedTab = remember { mutableStateOf<BottomNavItem>(BottomNavItem.Mission) }
+
+                Scaffold(
+                    bottomBar = {
+                        NavigationBar(selectedTab.value) { selectedTab.value = it }
+                    }
+                ) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        when (selectedTab.value) {
+                            BottomNavItem.Mission -> MissionScreen()
+                            BottomNavItem.Friends -> FriendsScreen()
+                            BottomNavItem.Test -> TestScreen()
+                            BottomNavItem.My -> MyScreen()
+                        }
+                    }
                 }
             }
         }
@@ -31,17 +48,84 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MissionScreen() {
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(text = "미션 화면")
+    }
+}
+
+@Composable
+fun FriendsScreen() {
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(text = "친구 화면")
+    }
+}
+
+@Composable
+fun TestScreen() {
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(text = "테스트 화면")
+    }
+}
+
+@Composable
+fun MyScreen() {
+    Box(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text(text = "마이 화면")
+    }
+}
+
+// Previews
+@Preview(showBackground = true)
+@Composable
+fun MainActivityPreview() {
+    OLOLTheme {
+        val selectedTab = remember { mutableStateOf<BottomNavItem>(BottomNavItem.Mission) }
+        Scaffold(
+            bottomBar = {
+                NavigationBar(selectedTab.value) { selectedTab.value = it }
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                when (selectedTab.value) {
+                    BottomNavItem.Mission -> MissionScreen()
+                    BottomNavItem.Friends -> FriendsScreen()
+                    BottomNavItem.Test -> TestScreen()
+                    BottomNavItem.My -> MyScreen()
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MissionScreenPreview() {
     OLOLTheme {
-        Greeting("Android")
+        MissionScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FriendsScreenPreview() {
+    OLOLTheme {
+        FriendsScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TestScreenPreview() {
+    OLOLTheme {
+        TestScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyScreenPreview() {
+    OLOLTheme {
+        MyScreen()
     }
 }
