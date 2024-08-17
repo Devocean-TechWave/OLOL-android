220adcba583a26733a3c1c1e9d4514e596769a2c
SeyeonJang <seye0n.bliss@gmail.com>
2024-08-17 13:35:08 +0900

    [#1] refactor: BottomNavigationView를 NavigationBar로 네이밍 변경

--- app/src/main/java/com/project/olol/presentation/ui/component/NavigationBar.kt
+++ app/src/main/java/com/project/olol/presentation/ui/component/NavigationBar.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import com.project.olol.R
 
