09b2f29370c30ba9e4236ba544f7b1be610045ae
SeyeonJang <seye0n.bliss@gmail.com>
2024-11-03 18:14:30 +0900

    [#12] refactor: AlbumFragment를 미션->앨범 폴더로 변경

--- app/src/main/java/com/project/olol/presentation/ui/album/AlbumFragment.kt
+++ app/src/main/java/com/project/olol/presentation/ui/album/AlbumFragment.kt
@@ -1,4 +1,4 @@
-package com.project.olol.presentation.ui.mission
+package com.project.olol.presentation.ui.album
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
--- app/src/main/java/com/project/olol/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/project/olol/presentation/ui/main/MainActivity.kt
@@ -9,7 +9,7 @@ import com.project.olol.R
 import com.project.olol.presentation.ui.component.BottomNavItem
 import com.project.olol.presentation.ui.component.NavigationBar
 import com.project.olol.presentation.ui.home.HomeFragment
-import com.project.olol.presentation.ui.mission.AlbumFragment
+import com.project.olol.presentation.ui.album.AlbumFragment
 import com.project.olol.presentation.ui.my.MyFragment
 import com.project.olol.presentation.ui.ranking.RankingFragment
 
